7267ced212efe0c0cfdab122c52a3739f0e0f733
thp <hong.ping.tan@gmail.com>
2018-07-27 14:22:20 +0800

    Initial commit with maven

---
+++ src/test/java/io/github/hongping/AppTest.java
@@ -0,0 +1,17 @@
+package io.github.hongping;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
